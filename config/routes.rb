6f38ec913fb79ac932a8b919e1ce5a69e80bba65
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2022-01-11 16:34:27 +0530

    Simple sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'dashboard#index'
+  get 'users/:id', to: 'dashboard#index', as: :user
 end
